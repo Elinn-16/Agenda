7739b27f228eccc36c952ae3e7df9ce32612ff95
chtelo <chtello@escuelasproa.edu.ar>
2023-05-29 15:09:37 -0300

    avances

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ const express = require('express');
 const app = express;
 const mongoose = require('mongoose')
 let port = 3000;
-const uri= "mongodb+srv:agendahector:hector@cluster0.53qoo2m.mongodb.net/?retryWrites=true&w=majority";
-
 
 app.listen(port, () => {
-    console.log(`conectados en el puerto : ${port}`)
+    console.log("tu servidor ha sido conectado correctamente");
 })
 app.get(('/') ,(req,res) =>{
 res.render("conectado");
 })
 
-mongoose. connect (uri, {useNewUrlParser: true,
-})
+mongoose. connect ("mongodb+srv:agendahector:hector@cluster0.53qoo2m.mongodb.net/?retryWrites=true&w=majority")
 .then (() => 
     console.log("conectado a la base de datos")
 )
 .catch((error) => 
     console.log("Tu error es  :" +error ));
+    
