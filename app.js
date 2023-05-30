0ac7e412f8561f14c057f3bb429e6e4d049230bd
chtelo <chtello@escuelasproa.edu.ar>
2023-05-30 01:20:47 -0300

    integracion de nuevas notas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express;
 const mongoose = require('mongoose')
+const c = document.getElementsByClassName("create")
 let port = 3000;
 
 app.listen(port, () => {
@@ -9,11 +10,20 @@ app.listen(port, () => {
 app.get(('/') ,(req,res) =>{
 res.render("conectado");
 })
-
+//crear una nueva nota
+function creit (c){
+    if (c) {
+    const NoteSchema =new mongoose.Schema({
+        Titulo :{type:String,require:"poner un titulo a la nota"},
+        Fecha:{type:Number},
+        Nota:{type:String,require:"poner el cuerpo de la nota"},
+    })
+}
+}
 mongoose. connect ("mongodb+srv:agendahector:hector@cluster0.53qoo2m.mongodb.net/?retryWrites=true&w=majority")
 .then (() => 
     console.log("conectado a la base de datos")
 )
 .catch((error) => 
     console.log("Tu error es  :" +error ));
-    
+
